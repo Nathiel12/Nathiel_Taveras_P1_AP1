44246a065fa19c584cdd3f7901719e98a9ff39f4
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-02-18 02:29:41 +0100

    Actualizar navegacion

--- Nathiel_Taveras_P1_AP1/Models/Aportes.cs
+++ Nathiel_Taveras_P1_AP1/Models/Aportes.cs
@@ -9,7 +9,7 @@ namespace Nathiel_Taveras_P1_AP1.Models
         [Required(ErrorMessage ="Este campo es requerido")]
         public string Persona { get; set; }
         [Required(ErrorMessage = "Este campo es requerido")]
-        public DateTime Fecha { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
         [Required(ErrorMessage = "Este campo es requerido")]
         public string Observacion { get; set; }
         [Range(1, double.MaxValue, ErrorMessage = "El monto tiene que ser mayor de 1")]
